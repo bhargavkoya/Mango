2f6ca0f390c885c56bc061a90f9960ddd2f04dfe
bhargav koya <bhargavkoya99@gmail.com>
2026-06-18 15:55:02 +0100

    Order API implemented with create order endpoint and consumed in the UI

---
+++ Mango.Web/Service/OrderService.cs
@@ -0,0 +1,43 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Service.IService;
+using Mango.Web.Utility;
+
+namespace Mango.Web.Service
+{
+    public class OrderService : IOrderService
+    {
+        private readonly IBaseService _baseService;
+        public OrderService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> CreateOrder(CartDto cartDto)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = cartDto,
+                Url = SD.OrderAPIBase + "/api/order/CreateOrder"
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllOrder(string? userId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.OrderAPIBase + "/api/order/GetOrders?userId=" + userId
+            });
+        }
+
+        public async Task<ResponseDto?> GetOrder(int orderId)
+        {
+            return await _baseService.SendAsync(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.OrderAPIBase + "/api/order/GetOrder/" + orderId
+            });
+        }
+    }
+}
